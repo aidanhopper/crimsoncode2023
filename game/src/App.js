2095a3d86dfd8ceccab03e939b953d7bacc3e8ba
Aidan Hopper <aidanhopper@pop-os.localdomain>
2023-02-18 15:03:23 -0800

    created a basic kahoot concpet with no functionality

--- game/src/App.js
+++ game/src/App.js
@@ -1,10 +1,11 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="jumbotron text-center">
       <header>
-        <h1>Tic Tac Toe in React!</h1>
       </header>
       <Game />
     </div>
@@ -12,65 +13,53 @@ function App() {
 }
 
 function Game() {
-  const grid = newTicTacToeGrid()
-
-  return <Grid grid={grid} />
-}
-
-function Grid ({ grid }) {
+  const [mode, setMode] = useState('start');
   return (
-        // Wrapping the grid with a div of inline-block means that the grid
-    // takes up only the space defined by the size of the cells, while
-    // still allowing us to use fractional values for the grid-template-*
-    // properties
-    <div style={{ display: 'inline-block' }}>
-      <div
-        style={{
-          // We set a background color to be revealed as the lines
-          // of the board with the `grid-gap` property
-          backgroundColor: '#000',
-          display: 'grid',
-          // Our rows are equal to the length of our grid
-          gridTemplateRows: `repeat(${grid.length}, 1fr)`,
-          // Our columns are equal to the length of a row
-          gridTemplateColumns: `repeat(${grid[0].length}, 1fr)`,
-          gridGap: 2,
-        }}
-      >
-        {grid.map((row, rowIdx) =>
-          row.map((cell, colIdx) => (
-            // We put the colIdx first because that is our X-axis value
-            // and the rowIdx second because that is our Y-axis value
-            // Getting in the habit makes using 2d grids much easier
-            <Cell key={`${colIdx}-${rowIdx}`} cell={cell} />
-          ))
-        )}
-      </div>
+    <div className="">
+      {mode === 'start' && (
+        <StartMenu onStartClick={() => setMode('kahoot')} />
+      )}
+      {mode === 'kahoot' && <Kahoot />}
+
     </div>
   )
 }
 
-const cellStyle = {
-  backgroundColor: '#fff',
-  height: 75,
-  width: 75,
+function StartMenu({ onStartClick }) {
+  return (
+    <div>
+      <button class="btn" onClick={ onStartClick }>
+        start game
+      </button>
+    </div>
+  )
 }
 
-function Cell({ cell }) {
-  return <div style={cellStyle}>{cell}</div>
+function Kahoot() {
+  let question1 = "asdf?";
+  let answer = 1;
+  let answers = ["yes", "no", "maybe", "so"];
+  return (
+    <>
+      <h1>{question1}</h1>
+      <div class="row">
+        <button class="btn btn-lg">
+          {answers[0]}
+        </button>
+        <button class="btn btn-lg">
+          {answers[1]}
+        </button>
+      </div>
+      <div class="row">
+        <button class="btn btn-lg">
+          {answers[2]}
+        </button>
+        <button class="btn btn-lg">
+          {answers[3]}
+        </button>
+      </div>
+    </>
+  )
 }
-
-function generateGrid(rows, columns, mapper) {
-  return Array(rows)
-    .fill()
-    .map(() =>
-      Array(columns)
-      .fill()
-      .map(mapper)
-    )
-}
-
-
-const newTicTacToeGrid = () => generateGrid(3, 3, () => null)
 
 export default App;

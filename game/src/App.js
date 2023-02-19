99e320cb9b8f21691bb5ca50c89bf207c3252b61
Aidan Hopper <aidanhopper@pop-os.localdomain>
2023-02-19 00:50:24 -0800

    stuff

--- game/src/App.js
+++ game/src/App.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
@@ -13,7 +14,30 @@ function App() {
 
 function Game() {
   const [mode, setMode] = useState('start');
-  const [questionIndex, setQuestion] = useState(0);
+  const [questionIndex, setQuestion] = useState(1);
+
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+     async function fetchData() {
+      const response = await fetch(`http://localhost:8000/api/question/?id=${questionIndex}`);
+      const data = await response.json();
+      //console.log(data[0]);
+       
+      let arr = [
+        data[0].question, [
+        data[0].option1,
+        data[0].option2,
+        data[0].option3,
+        data[0].option4,
+      ],
+      data[0].correct_option
+      ];
+      setData(arr);
+     };
+    fetchData();
+  }, [questionIndex]);
+
 
   let questions = [
     ["What color is the sky?", ["red", "blue", "green", "orange"], 1],
@@ -21,7 +45,8 @@ function Game() {
     ["What is the capitol of Canada", ["Calgary", "Kamloops", "Vancouver", "Montreal"], 3],
   ];
 
-  let question = questions[questionIndex];
+  console.log(data);
+  //let question = questions[questionIndex];
 
   return (
     <div className="">
@@ -30,13 +55,13 @@ function Game() {
       )}
       {mode === 'question' && (
         <Question goToAnswerScreen={() => setMode('answer')}
-        question = {() => {return question}}
+        question = {() => {return data}}
         />
       )}
       {mode === 'answer' && (
         <Answer goToQuestionScreen={() => setMode('question')} 
         nextQuestion = {() => setQuestion(questionIndex + 1)}
-        question = {() => {return question}}
+        question = {() => {return data}}
        />
       )}
     </div>

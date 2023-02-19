b00c724ac87248d1c61e1712157c5fea2556235b
Aidan Hopper <aidanhopper@pop-os.localdomain>
2023-02-18 23:07:41 -0800

    removed django

--- oursite/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Question
-
-
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = ('id', 'question', 'option1', 'option2', 'option3', 'option4', 'correct_option')
--- oursite/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('question/', views.QuestionView.as_view(), name='question'),
-]

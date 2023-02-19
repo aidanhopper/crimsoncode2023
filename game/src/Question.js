661017c81e5677855a1acf28476d00014384e12b
Leo Curdi <ldcurdi@icloud.com>
2023-02-19 02:15:53 -0800

    updated

--- game/src/Question.js
+++ game/src/Question.js
@@ -12,23 +12,23 @@ export default function Question({ goToAnswerScreen, question }) {
 
   return (
     <div className="container">
+    <h1 class="w-full bg-blue-200 text-white py-4 px-6 rounded-md hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 height-full">{question()[0]}</h1>
+      <div class="flex justify-between">
+        <button className=" w-1/2 bg-blue-500 text-white py-2 px-4 hover:bg-blue-700" onClick={() => setAnswerClicked(0) }>
+          {question()[1][0]}
+        </button>
+        <button className="w-1/2 bg-blue-500 text-white py-2 px-4 hover:bg-blue-700" onClick={() => setAnswerClicked(1)}>
+          {question()[1][1]} 
+        </button>
+      </div>
       <div className="row">
-      <h1>{question()[0]}</h1>
-      <button className={ansBtn} onClick={() => setAnswerClicked(0) }>
-        {question()[1][0]}
-      </button>
-      <button className={ansBtn} onClick={() => setAnswerClicked(1)}>
-        {question()[1][1]} 
-      </button>
-    </div>
-    <div className="row">
-      <button className={ansBtn} onClick={() => setAnswerClicked(2)}>
-        {question()[1][2]}
-      </button>
-      <button className={ansBtn} onClick={() => setAnswerClicked(3)}>
-        {question()[1][3]}
-      </button>
-    </div>
+        <button className="w-1/2 bg-blue-500 text-white py-2 px-4 hover:bg-blue-700" onClick={() => setAnswerClicked(2)}>
+          {question()[1][2]}
+        </button>
+        <button className="w-1/2 bg-blue-500 text-white py-2 px-4 hover:bg-blue-700" onClick={() => setAnswerClicked(3)}>
+          {question()[1][3]}
+        </button>
+      </div>
     </div>
   )
 }
--- game/src/StartMenu.js
+++ game/src/StartMenu.js
@@ -1,9 +1,9 @@
 
 export default function StartMenu({ onStartClick }) {
   return (
-    <div>
-      <button className="btn" onClick={ onStartClick }>
-        start game
+    <div class="h-screen flex items-center justify-center bg-blue-500">
+      <button className="text-6xl px-16 py-16 bg-blue-500 text-white rounded-full hover:bg-blue-700" onClick={ onStartClick }>
+        <span>start game</span>
       </button>
     </div>
   )

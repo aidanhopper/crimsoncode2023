6c0290d7c73df98afe040248144091901d80a939
Leo Curdi <ldcurdi@icloud.com>
2023-02-19 02:30:44 -0800

    beautified

--- game/src/StartMenu.js
+++ game/src/StartMenu.js
@@ -3,7 +3,7 @@ export default function StartMenu({ onStartClick }) {
   return (
     <div class="h-screen flex items-center justify-center bg-blue-500">
       <button className="text-6xl px-16 py-16 bg-blue-500 text-white rounded-full hover:bg-blue-700" onClick={ onStartClick }>
-        <span>start game</span>
+        <span>play</span>
       </button>
     </div>
   )
